f12b32382cb7f98480c1d3becace2793402a0454
harshita1611 <97826462+harshita1611@users.noreply.github.com>
2023-07-17 04:08:11 +0530

    regis

--- FrontEnd/src/pages/Registration.jsx
+++ FrontEnd/src/pages/Registration.jsx
@@ -1,16 +1,22 @@
 import Form from "../components/Form"
+import alumni from "../images/alumni.webp"
 export default function Registration() {
     return(
         <div>
-            <h1 className="flex justify-center text-3xl mb-5">Registration</h1>
-            <Form
-                username="true"
-                password="true"
-                firstName="true"
-                lastName="true"
-                email="true"
-                confirmPassword="true"
-            />
+            <h1 className="flex justify-center text-3xl ">Registration</h1>
+            <div className="flex p-10">
+                <div>
+                    <img src={alumni} className="w-11/12 ml-12"/>
+                </div>
+                <Form 
+                    username="true"
+                    password="true"
+                    firstName="true"
+                    lastName="true"
+                    email="true"
+                    confirmPassword="true"
+                />
+                </div>
         </div>
     )
 }
