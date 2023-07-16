da73fe96a319860bede6dbfd46a3f3d492db4426
harshita1611 <97826462+harshita1611@users.noreply.github.com>
2023-07-16 16:57:59 +0530

    NavbarStyling

--- FrontEnd/src/components/Navbar.jsx
+++ FrontEnd/src/components/Navbar.jsx
@@ -1,34 +1,32 @@
 import { Link,NavLink } from 'react-router-dom'
-
+import SearchBar from './Searchbar'
 export default function Navbar(props){
 
-    const btn="hover:bg-[#286dc0] px-5 py-2 duration-300 rounded"
+    const btn="hover:bg-[#286dc0] px-5 py-2 duration-300 rounded hover:text-white "
     return(
         <>
-        <div className='px-48 '>
+        <div className='px-20 '>
 
             <div className='flex justify-between h-44 items-center '  >
                 <div className='place-items-start'>
                     {(props.alumni)&& <NavLink to={props.alumni} className='text-6xl text-[#00356B]'>
-                        <button >alumni</button>
+                        <button className='font-bold' >Alumni</button>
                     </NavLink>}
                 </div> 
-                {/* <div>
-                    search
-                </div> */}
+                <SearchBar/>
                 <div className='flex justify-end text-2xl text-[#00356B]'>
 
                     {(props.login)&& <NavLink to={props.login} className={btn}>
-                        <button >login</button>
+                        <button className='p-2'>Login</button>
                     </NavLink>}
                     {(props.events) && <NavLink to={props.events} className={btn}>
-                        <button>events</button>
+                        <button className='p-2'>Events</button>
                     </NavLink>}
                     {(props.about) && <NavLink to={props.about} className={btn}>
-                        <button>about</button>
+                        <button className='p-2'>About</button>
                     </NavLink>}
                     {(props.home) && <NavLink to={props.home} className={btn}>
-                        <button>news</button>
+                        <button className='p-2'>News</button>
                     </NavLink>}
                 </div>
                 
---
+++ FrontEnd/src/components/Searchbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {AiOutlineSearch} from 'react-icons/ai'
+const SearchBar = () => {
+  return (
+    <div className='flex h-10 px-5'>
+      <input type="search" placeholder="Search" className=' border-2 border-gray-500 bg-white h-10 px-5 w-80 rounded-lg text-sm focus:outline-none' />
+      <AiOutlineSearch size={30} className='ml-1 mt-1'/>
+    </div>
+  )
+}
+
+export default SearchBar
