81d023d808b864e0f64f6437a8a8afdaecc04005
Devvrat1010 <rathoddevvratsingh777@gmail.com>
2023-07-17 02:08:55 +0530

    unheee

--- FrontEnd/src/components/createEvent.jsx
+++ FrontEnd/src/components/createEvent.jsx
@@ -1,8 +1,15 @@
+import Form from "./Form"
+
 export default function CreateEvent(){
     return(
         <div>
             <Form
-                
+                title="true"
+                description="true"
+                category="true"
+                date="true"
+                time="true"
+                contact="true"
             />
         </div>
     )
--- FrontEnd/src/pages/Events.jsx
+++ FrontEnd/src/pages/Events.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Events = (props) => {
+    const navigate = useNavigate()
+    const createEvent = () => {
+        console.log(props)
+        navigate('/collegeHome/createEvent')
+    }
+
+
+    const btn="hover:bg-[#286dc0] px-5 py-2 duration-300 rounded hover:text-white  text-2xl hover:cursor-pointer"
   return (
     <div className='flex justify-center m-auto'>
       <div className='p-5'>
@@ -11,6 +20,9 @@ const Events = (props) => {
         <h1> Description: {props.description}</h1>
         <h1> Time: {props.time}</h1>
       </div>
+      <button className={btn} onClick={createEvent}>
+        CreateEvent
+      </button>
     </div>
   )
 }
